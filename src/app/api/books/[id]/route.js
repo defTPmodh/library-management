2f988710fba7a4deea1f10df9153619a3e0b8e6a
Tanish Modh <tpmodh@gmail.com>
2025-01-19 22:01:48 +0400

    Update route.js

--- src/app/api/books/[id]/route.js
+++ src/app/api/books/[id]/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
-import prisma from '../../../lib/prisma';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 
 export async function GET(request) {
   try {
